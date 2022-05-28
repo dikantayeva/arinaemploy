e588a94193b8598ac060ab5b20a224296b8f35b2
dikantayeva <63303303+dikantayeva@users.noreply.github.com>
2022-05-28 17:19:18 +0600

    Update settings.py

--- employee_management/settings.py
+++ employee_management/settings.py
@@ -121,3 +121,6 @@ STATIC_URL = '/static/'
 import dj_database_url 
  prod_db = dj_database_url.config(conn_max_age=500) 
  DATABASES['default'].update(prod_db)  
+
+ static files for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
